a82721efa34ad7b2dcbed3a5b62ee09a9e46f268
Diego Adolfo <diegoado@gmail.com>
2015-04-18 10:24:36 -0300

    Questão 1429 accepted!

---
+++ accepted - Math/FatorialNovo.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+ 
+# Universidade Federal de Campina Grande
+# Aluno: Diego Adolfo Silva de Araújo
+# Matricula: 113210090
+# Disciplina: Algoritmos Avançados
+  
+# Problema: Fatorial de Novo!
+# Nivel: 3
+
+def fatorial(n):
+    if n < 1:
+        return 1
+    
+    return n * fatorial(n - 1)
+
+while 1:
+    n = raw_input()
+    if n == '0': break
+    
+    dec = 0
+    x = len(n)
+    for i in range(x):
+        dec += int(n[i]) * fatorial(x)
+        x -= 1
+    
+    print dec
