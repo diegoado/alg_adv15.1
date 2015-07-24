71e27911bb7bb9d7fca72c4784d183a92f968be5
Diego Adolfo <diegoado@gmail.com>
2015-07-23 23:21:42 -0300

    New Repository

---
+++ mix1/LalaLandAndAppleTrees.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Universidade Federal de Campina Grande
+# Aluno: Diego Adolfo Silva de Araújo
+# Matricula: 113210090
+# Disciplina: Algoritmos Avançados
+
+# Codeforces
+# Problem: 558A - A. Lala Land and Apple Trees
+# Time limit per test: 1 second
+# Memory limit per test: 256 megabytes
+# Input: standard input
+# Output: standard output
+
+
