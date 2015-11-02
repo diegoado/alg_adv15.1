d7a45e5505f94c0d201edf917a5d57cef9b3f36d
Diego Adolfo <diegoado@gmail.com>
2015-11-02 15:46:13 -0300

    problem 371B accept

---
+++ revisao1/FoxDividingCheese.cpp
@@ -0,0 +1,57 @@
+/*
+ * University Federal of Campina Grande
+ * Student: Diego Adolfo Silva de Araújo
+ * Registry: 113210090
+ * Discipline: Algoritmos Avançados
+ *
+ * Code forces
+ * Problem: 371B - B. Fox Dividing Cheese
+ * Time limit per test: 1 second
+ * Memory limit per test: 256 megabytes
+ * Input: standard input
+ * Output: standard output
+ */
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int auxcalc(int &x, int div) {
+	int count = 0;
+	while(x % div == 0) {
+		x /= div;
+		count++;
+	}
+	return count;
+}
+
+void calc(int &a, int &b, int &step) {
+	step += abs(auxcalc(a, 2) - auxcalc(b, 2)) +
+			abs(auxcalc(a, 3) - auxcalc(b, 3)) +
+			abs(auxcalc(a, 5) - auxcalc(b, 5));
+}
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+
+	if(a == b)
+	{
+		cout << 0 << endl;
+	} else
+	{
+		int step = 0;
+		calc(a, b, step);
+		if(a == b)
+		{
+			cout << step << endl;
+		}
+		else
+		{
+			cout << -1 << endl;
+		}
+	}
+	return 0;
+}
+
